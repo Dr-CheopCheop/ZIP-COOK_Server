8facd1e7bfdbfd4c6f7a65fae55f6645f68fd64c
leegayeong <leegy0419@gmail.com>
2023-04-03 23:37:57 +0900

    feat: initial setup for project

---
+++ src/main/java/com/zipcook_server/data/entity/ExEntity.java
@@ -0,0 +1,7 @@
+package com.zipcook_server.data.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ExEntity {
+}
---
+++ src/test/java/com/zipcook_server/ZipCookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcook_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipCookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
