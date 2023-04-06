0209b6481e36b6699858e8266549cfcc8f4d17d2
이승희 <seunghee127@naver.com>
2023-04-06 09:08:55 +0900

    Docs: ex제거

--- src/main/java/com/zipcook_server/data/entity/ExEntity.java
+++
@@ -1,7 +0,0 @@
-package com.zipcook_server.data.entity;
-
-import javax.persistence.Entity;
-
-@Entity
-public class ExEntity {
-}
--- src/main/java/com/zipcook_server/data/repository/ExRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zipcook_server.data.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ExRepository {
-}
