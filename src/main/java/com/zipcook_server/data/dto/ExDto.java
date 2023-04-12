d22d5408b70763cfabe784b424d9348a160cf5f5
GaYeongLee0419 <68856259+GaYeongLee0419@users.noreply.github.com>
2023-04-12 23:42:18 +0900

    Revert "OpenAI GPT-3를 이용한 챗봇 기능 "

--- src/main/java/com/zipcook_server/data/dto/ExDto.java
+++
@@ -1,4 +0,0 @@
-package com.zipcook_server.data.dto;
-
-public class ExDto {
-}
--- src/main/java/com/zipcook_server/service/ExService.java
+++
@@ -1,7 +0,0 @@
-package com.zipcook_server.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExService {
-}
