ea39c24c9c92db3b8ca81f89f9c2bd630ac56989
이승희 <seunghee127@naver.com>
2023-04-30 20:00:12 +0900

    Docs : @transactional 추가

--- src/test/java/com/zipcook_server/controller/ShareControllerTest.java
+++ src/test/java/com/zipcook_server/controller/ShareControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@Transactional
 @SpringBootTest
 class ShareControllerTest {
 
