ee1b8b7700a1ef47d74de4685e45e36630303ece
leegayeong <leegy0419@gmail.com>
2023-06-05 20:22:56 +0900

    #17 임시비밀번호 대소문자, 특수문자 섞어서 발급으로 수정

--- src/main/java/com/zipcook_server/service/MailService.java
+++ src/main/java/com/zipcook_server/service/MailService.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class MailService {
@@ -41,25 +42,71 @@ public class MailService {
         }
     }
 
-    public UserDTO sendMail(String username){
+//    public UserDTO sendMail(String username){
+//
+//        Optional<User> user1 = userRepository.findOneWithAuthoritiesByUsername(username);
+//
+//        Optional<UserDTO> userDTO = Optional.ofNullable(UserDTO.from(user1.get()));
+//        String email = userDTO.get().getEmail();
+//
+//        if(user1.isPresent()) {
+//
+//            String pw = "";
+//            for (int i = 0; i < 12; i++) {
+//                pw += (char) ((Math.random() * 26) + 97);
+//            }
+//
+//            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//
+//            simpleMailMessage.setTo(email);
+//            simpleMailMessage.setSubject("임시 비밀번호 발급 안내 이메일");
+//            simpleMailMessage.setText(pw);
+//
+//            User user = User.builder()
+//                    .userId(user1.get().getUserId())
+//                    .username(user1.get().getUsername())
+//                    .email(email)
+//                    .location(user1.get().getLocation())
+//                    .nickname(user1.get().getNickname())
+//                    .password(passwordEncoder.encode(pw))
+//                    .authorities(user1.get().getAuthorities())
+//                    .build();
+//
+//            javaMailSender.send(simpleMailMessage);
+//
+//            Optional<UserDTO> dto = Optional.ofNullable(UserDTO.from(user));
+//
+//            userRepository.save(user);
+//
+//            return dto.get();
+//        }
+//        else {
+//            return userDTO.get();
+//        }
+//    }
 
+    public UserDTO sendMail(String username) {
         Optional<User> user1 = userRepository.findOneWithAuthoritiesByUsername(username);
 
-        Optional<UserDTO> userDTO = Optional.ofNullable(UserDTO.from(user1.get()));
-        String email = userDTO.get().getEmail();
+        if (user1.isPresent()) {
+            String email = user1.get().getEmail();
 
-        if(user1.isPresent()) {
+            // 대소문자, 숫자, 특수문자를 포함한 문자셋
+            String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
 
-            String pw = "";
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
             for (int i = 0; i < 12; i++) {
-                pw += (char) ((Math.random() * 26) + 97);
+                int index = random.nextInt(characters.length());
+                char character = characters.charAt(index);
+                password.append(character);
             }
 
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-
             simpleMailMessage.setTo(email);
             simpleMailMessage.setSubject("임시 비밀번호 발급 안내 이메일");
-            simpleMailMessage.setText(pw);
+            simpleMailMessage.setText(password.toString());
 
             User user = User.builder()
                     .userId(user1.get().getUserId())
@@ -67,20 +114,17 @@ public class MailService {
                     .email(email)
                     .location(user1.get().getLocation())
                     .nickname(user1.get().getNickname())
-                    .password(passwordEncoder.encode(pw))
+                    .password(passwordEncoder.encode(password.toString()))
                     .authorities(user1.get().getAuthorities())
                     .build();
 
             javaMailSender.send(simpleMailMessage);
 
-            Optional<UserDTO> dto = Optional.ofNullable(UserDTO.from(user));
-
             userRepository.save(user);
 
-            return dto.get();
-        }
-        else {
-            return userDTO.get();
+            return UserDTO.from(user);
+        } else {
+            return UserDTO.from(user1.orElse(null));
         }
     }
 
